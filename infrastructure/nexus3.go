dd079fdb26e64acf08322eb615cafd957d37d661
CloudoguNguyen <thanh.long.nguyen@cloudogu.com>
2018-04-30 15:02:24 +0200

    Updated nexus3.go and create-Repository.groovy

--- infrastructure/nexus3.go
+++ infrastructure/nexus3.go
@@ -10,10 +10,6 @@ import (
 
 	"encoding/json"
 
-	"log"
-
-	"io"
-	"bytes"
 	"github.com/cloudogu/nexus-claim/domain"
 	"github.com/pkg/errors"
   "github.com/cloudogu/nexus-scripting/manager"
@@ -62,47 +58,6 @@ func (client *httpNexus3APIClient) Get(repositoryType domain.RepositoryType, id 
 
 }
 
-func (client *httpNexus3APIClient) createRepositoryURL(repositoryType domain.RepositoryType, id domain.RepositoryID) string {
-	return client.createRepositoryServiceURL(repositoryType) + "/" + string(id)
-}
-
-func (client *httpNexus3APIClient) createReadRequest(url string) (*http.Request, error) {
-	request, err := client.createRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	request.Header.Set("Accept", contentType)
-	return request, nil
-}
-
-func (client *httpNexus3APIClient) createRequest(method string, url string, body io.Reader) (*http.Request, error) {
-	request, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to create %s request for %s", method, url)
-	}
-
-	if client.username != "" {
-		request.SetBasicAuth(client.username, client.password)
-	}
-
-	return request, nil
-}
-
-func (client *httpNexus3APIClient) closeAndLogOnError(closer io.Closer) {
-	err := closer.Close()
-	if err != nil {
-		log.Printf("failed to close response body: %v", err)
-	}
-}
-
-func (client *httpNexus3APIClient) isStatusNotFound(response *http.Response) bool {
-	return response.StatusCode == 404
-}
-
-func (client *httpNexus3APIClient) isStatusOK(response *http.Response) bool {
-	return response.StatusCode == 200
-}
-
 func (client *httpNexus3APIClient) parseRepositoryJson(jsonData string) (*domain.Repository, error) {
 
   fmt.Println(jsonData)
@@ -124,99 +79,28 @@ func (client *httpNexus3APIClient) parseRepositoryJson(jsonData string) (*domain
 
 
 func (client *httpNexus3APIClient) Create(repository domain.Repository) error {
-	dto := newRepositoryDTO().from(repository)
-	request, err := client.createWriteRequest("POST", client.createRepositoryServiceURL(repository.Type), dto)
-	if err != nil {
-		return err
-	}
-
-	response, err := client.httpClient.Do(request)
-	if err != nil {
-		return errors.Wrapf(err, "failed to create repository %s", repository.ID)
-	}
-
-	if response.StatusCode != 201 {
-		return client.statusCodeError(response.StatusCode)
-	}
-
 	return nil
-}
-
-func (client *httpNexus3APIClient) createRepositoryServiceURL(repositoryType domain.RepositoryType) string {
-	var typeURLPart string
-	switch repositoryType {
-	case domain.TypeRepository:
-		typeURLPart = "repositories"
-	case domain.TypeGroup:
-		typeURLPart = "repo_groups"
-	}
-	return client.url + repositoryServiceURL + typeURLPart
-}
-
-func (client *httpNexus3APIClient) createWriteRequest(method, url string, body interface{}) (*http.Request, error) {
-	reader, err := client.createJSONBody(body)
-	if err != nil {
-		return nil, err
-	}
-
-	request, err := client.createRequest(method, url, reader)
-	if err != nil {
-		return nil, err
-	}
-	request.Header.Set("Content-Type", contentType)
-	return request, nil
-}
-
-func (client *httpNexus3APIClient) createJSONBody(object interface{}) (io.Reader, error) {
-	data, err := json.Marshal(object)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to marshal object")
-	}
-
-	return bytes.NewBuffer(data), nil
 }
 
 func (client *httpNexus3APIClient) Modify(repository domain.Repository) error {
-	dto := newRepositoryDTO().from(repository)
-	request, err := client.createWriteRequest("PUT", client.createRepositoryURL(repository.Type, repository.ID), dto)
-	if err != nil {
-		return err
-	}
-
-	response, err := client.httpClient.Do(request)
-	if err != nil {
-		return errors.Wrapf(err, "failed to modify repository %s", repository.ID)
-	}
-
-	if response.StatusCode != 200 {
-		return client.statusCodeError(response.StatusCode)
-	}
-
 	return nil
-}
-
-func (client *httpNexus3APIClient) statusCodeError(statusCode int) error {
-	return errors.Errorf("invalid status code %d", statusCode)
 }
 
 func (client *httpNexus3APIClient) Remove(repository domain.Repository) error {
-	id := repository.ID
 
-	request, err := client.createRequest("DELETE", client.createRepositoryURL(repository.Type, id), nil)
-	if err != nil {
-		return err
-	}
+  deleteRepositoryScript := DELETE_REPOSITORY;
+  StringID := string(repository.ID)
+  script,err := client.manager.Create("deleteRepository",deleteRepositoryScript)
+  if err != nil {
+    return err
+  }
 
-	response, err := client.httpClient.Do(request)
-	if err != nil {
-		return errors.Wrapf(err, "failed to remove repository %s", id)
-	}
+  _, err = script.ExecuteWithStringPayload(StringID)
+  if err != nil {
+    return err
+  }
 
-	if response.StatusCode != 204 {
-		return client.statusCodeError(response.StatusCode)
-	}
-
-	return nil
+  return nil
 }
 
 func newRepository3DTO() *repository3DTO {
--- scripts/create-Repository.groovy
+++ scripts/create-Repository.groovy
@@ -3,83 +3,110 @@ import org.sonatype.nexus.blobstore.api.BlobStoreManager
 import org.sonatype.nexus.repository.config.Configuration
 import org.sonatype.nexus.repository.storage.WritePolicy
 
-
+/*
+  WORK IN PROGRESS
+ */
 class Repository {
-    Map<String, Map<String, Object>> properties = new HashMap<String, Object>()
-}
-
-def getRepositories(String data) {
-
-    def inputJson = new JsonSlurper().parseText(data)
-    Repository repo = new Repository()
-    inputJson.each {
-        if(it.key.toString().toLowerCase().contains("blobstorename")){
-            repo.properties.put(it.key,getBlobStoreName(it.value))
-        }
-        else if (it.key.toString().toLowerCase().contains("writepolicy")){
-            repo.properties.put(it.key,getWritePolicy(it.value))
-        }
-        else {
-            repo.properties.put(it.key, it.value)
-        }
-    }
- return repo
+  Map<String, Map<String, Object>> properties = new HashMap<String, Object>()
 
 }
 
-def getBlobStoreName(String value) {
-    switch (value.toLowerCase()) {
-        case "default" || "" || null: return BlobStoreManager.DEFAULT_BLOBSTORE_NAME
-    }
-    return value
+def getDefaultAttributeValues(String type, String remoteURL){
+
+  Map<String, Map<String, Object>> defaultAttributeValues = new HashMap<String,Object>()
+
+  if (type.equals("hosted")) {
+
+    Map<String,Object> storage = new HashMap<String,Object>()
+    storage.put("blobStoreName",BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
+    storage.put("writePolicy",WritePolicy.ALLOW)
+    storage.put("strictContentTypeValidation",false)
+    defaultAttributeValues.put("attributes",storage)
+  }
+  else if (type.equals("proxy")){
+    Map<String,Object> httpClient = new HashMap<String,Object>()
+    Map<String,Object> connection = new HashMap<String,Object>()
+    connection.put("blocked",false)
+    connection.put("autoBlock",true)
+    httpClient.put("connection",connection)
+    defaultAttributeValues.put("httpclient",httpClient)
+
+    Map<String,Object> proxy = new HashMap<String,Object>()
+    proxy.put("remoteUrl",remoteURL)
+    proxy.put("contentMaxAge",1440)
+    proxy.put("metadataMaxAge",1440)
+    defaultAttributeValues.put("proxy",proxy)
+
+    Map<String,Object> negativeCache = new HashMap<String,Object>()
+    negativeCache.put("enabled",true)
+    negativeCache.put("timeToLive",1440)
+    defaultAttributeValues.put("negativeCache",negativeCache)
+
+    Map<String,Object> storage = new HashMap<String,Object>()
+    storage.put("blobStoreName",BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
+    storage.put("strictContentTypeValidation",false)
+    defaultAttributeValues.put("attributes",storage)
+
+  }
+  else if(type.equals("group")){
+    Map<String,Object> storage = new HashMap<String,Object>()
+    storage.put("blobStoreName",BlobStoreManager.DEFAULT_BLOBSTORE_NAME)
+    defaultAttributeValues.put("attributes",storage)
+
+  }
+
+  return defaultAttributeValues
 }
 
-def getWritePolicy(String value){
-    switch (value.toLowerCase()){
-        case "default" || "allow" || "" || null: return WritePolicy.ALLOW
-        case "allow_once" : return WritePolicy.ALLOW_ONCE
-        case "deny" : return WritePolicy.DENY
-    }
-    return value
+
+def convertJsonFileToRepo(String jsonData) {
+
+  def inputJson = new JsonSlurper().parseText(jsonData)
+  Repository repo = new Repository()
+  inputJson.each {
+    repo.properties.put(it.key, it.value)
+  }
+
+  return repo
 }
 
-
-// Work in progress
 def createRepository(Repository repo) {
 
-    String name = repo.properties.get("name")
-    repo.properties.remove("name")
 
-    String recipeName = repo.properties.get("recipeName")
-    repo.properties.remove("recipeName")
+  String name = repo.properties.get("name")
+  repo.properties.remove("name")
 
-    String state = repo.properties.get("_state")
-    repo.properties.remove("_state")
+  String recipeName = repo.properties.get("recipeName")
+  repo.properties.remove("recipeName")
+
+  String state = repo.properties.get("_state")
+  repo.properties.remove("_state")
 
 
 
-    Configuration conf = new Configuration(
-            repositoryName: name,
-            recipeName: recipeName,
-            online: repo.properties.get("online"),
-            attributes: [
-                    storage: [
-                            blobStoreName              : repo.properties.get("blobStoreName"),
-                            writePolicy                : writePolicy,
-                            strictContentTypeValidation: strictContentTypeValidation
-                    ] as Map
-            ] as Map
-    )
+  Configuration conf = new Configuration(
+    repositoryName: name,
+    recipeName: recipeName,
+    online: repo.properties.get("online"),
+    attributes: [
+      storage: [
+        blobStoreName              : repo.properties.get("blobStoreName"),
+        writePolicy                : writePolicy,
+        strictContentTypeValidation: strictContentTypeValidation
+      ] as Map
+    ] as Map
+  )
 
-    conf.setAttributes()
+  conf.setAttributes()
 
-    repository.createRepository(conf)
+  repository.createRepository(conf)
 
 }
+
 
 if (args != "") {
-    def rep = getRepositories(args)
-    createRepository(rep)
+
+  def rep = convertJsonFileToRepo(args)
+  createRepository(rep)
+
 }
-
-
