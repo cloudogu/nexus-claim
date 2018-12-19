a7f1ed1ef1df39373a5f8d15b87f2cd2297a15c5
CloudoguNguyen <thanh.long.nguyen@cloudogu.com>
2018-12-19 13:39:22 +0100

    Update createRepo groovy so if maven is not set in the .hcl, default values will be applied

--- infrastructure/groovy_scripts.go
+++ infrastructure/groovy_scripts.go
@@ -11,7 +11,6 @@ class Repository {
 if (args != "") {
 
   def rep = convertJsonFileToRepo(args)
-
   def output = createRepository(rep)
 
   return output
@@ -25,7 +24,7 @@ def createRepository(Repository repo) {
   try {
     repository.createRepository(conf)
   }
-  catch (Exception e){
+  catch (Exception e) {
     return e
   }
 
@@ -36,6 +35,7 @@ def convertJsonFileToRepo(String jsonData) {
 
   def inputJson = new JsonSlurper().parseText(jsonData)
   Repository repo = new Repository()
+
   inputJson.each {
     repo.properties.put(it.key, it.value)
   }
@@ -43,61 +43,112 @@ def convertJsonFileToRepo(String jsonData) {
   return repo
 }
 
-def configureAttributes(Map<String, Object> properties){
 
-  for (pro in properties){
-
-    if (pro.key.equals("httpclient")){
-
-      HashMap<String,Object> mapValue = pro.value.get(0)
-
-      mapValue = configureAttributes(mapValue)
-
-      pro.setValue(mapValue)
-      properties.put(pro.key, pro.value)
-
-    }
-
-    else {
-      properties.put(pro.key, pro.value.get(0))
-
-    }
-
-  }
-  return properties
-}
-
-def createConfiguration(Repository repo){
+def createConfiguration(Repository repo) {
 
   def name = getName(repo)
   def recipeName = getRecipeName(repo)
   def online = getOnline(repo)
   def attributes = repo.properties.get("attributes")
 
-
   attributes = configureAttributes(attributes)
+
+  att = setDefaultValuesOnNull(attributes)
 
   Configuration conf = new Configuration(
     repositoryName: name,
     recipeName: recipeName,
     online: online,
-    attributes: attributes
+    attributes: att
   )
 
   return conf
 }
 
-def getName(Repository repo){
+def setDefaultValuesOnNull(Map<String, Object> attributes) {
+
+  Map<String, Object> result = attributes
+
+  for (att in attributes) {
+
+    if (att.key.equals("maven")) {
+
+      HashMap<String, Object> mavenEntry = att.value
+      mavenEntry = replaceMavenEntryIfNull(mavenEntry)
+      result.put(att.key, mavenEntry)
+    }
+  }
+  return result
+}
+
+
+def configureAttributes(Map<String, Object> attributes) {
+
+  for (att in attributes) {
+
+    if (att.key.equals("httpclient")) {
+
+      HashMap<String, Object> mapValue = att.value.get(0)
+
+      mapValue = configureAttributes(mapValue)
+
+      att.setValue(mapValue)
+      attributes.put(att.key, att.value)
+
+    } else {
+      attributes.put(att.key, att.value.get(0))
+
+    }
+
+  }
+  return attributes
+}
+
+def isMapEmpty(Map <String, Object> entry ){
+
+  if (entry.size() == 0) {
+    return true
+  }
+
+  entry.each {
+    if (!it.value.equals("")){
+      return false
+    }
+  }
+
+  return true
+
+}
+
+def replaceMavenEntryIfNull(Map<String, Object> entry) {
+
+  if (isMapEmpty(entry)) {
+    entry = getDefaultMavenEntry()
+  }
+  return entry
+
+}
+
+def getDefaultMavenEntry() {
+  Map<String, String> mavenEntry = new HashMap<>()
+
+  mavenEntry.put("layoutPolicy", "STRICT")
+  mavenEntry.put("versionPolicy", "RELEASE")
+
+  return mavenEntry
+}
+
+def getName(Repository repo) {
   String name = repo.getProperties().get("repositoryName")
   return name
 }
 
-def getRecipeName(Repository repo){
+def getRecipeName(Repository repo) {
   String recipeName = repo.getProperties().get("recipeName")
   return recipeName
 }
 
-def getOnline(Repository repo){
+def getOnline(Repository repo) {
   String online = repo.getProperties().get("online")
   return online
 }
